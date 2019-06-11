1ce8739905ed22125bd74f6426068a2ec5b5e4cc
Will Acton <will@willacton.com>
2019-06-11 08:09:15 -0700

    project.clj and deps.edn

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lilactown/hiccup-tag "0.0.1"
+  :description "A library for parsing hiccup forms using reader tagged literals."
+  :url "https://github.com/Lokeh/hiccup-tag"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v20.html"}
+  :source-paths ["src"])
